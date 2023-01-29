a3608e9fad0c9283abd663f973a4cc0f1ce8deb0
Amelia Bigler <ameliabigler@Amelias-Air.augiemagic.home>
2023-01-29 16:21:03 -0600

    add renderFavorites function and improve renderSearch function. add empty method to vid containers.

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,9 @@ $(function (){
     var favoritesDisplay = $('#favorites-display');
     var resultsDisplay = $('#results-display')
 
+    var resultsGrandparent = $('#searchVids')
+    var favoritesGrandparent = $('#favoriteVids')
+
     var searchBtn = $('#search');
     searchBtn.on('click', function(event){
         event.preventDefault();
@@ -14,6 +17,8 @@ $(function (){
 
         homeDisplay.hide();
         resultsDisplay.show();
+
+        renderSearch(); //TODO: relocate this function call to API function once it's created.
     })
 
     var backBtn = $('#back');
@@ -22,55 +27,98 @@ $(function (){
         
         homeDisplay.show();
         resultsDisplay.hide();
+
+        resultsGrandparent.children().children().empty();
     })
 
     var favorites = $('#favorites-home');
     favorites.on('click', function(){
 
         if (favorites.text() === 'Favorites'){
-            // TODO: call renderFavorites function
+            resultsGrandparent.children().children().empty();
+            renderFavorites();
             homeDisplay.hide();
+            resultsDisplay.hide();
             favoritesDisplay.show();
             favorites.text('Home');
         } 
         else if (favorites.text() === 'Home'){
-            // TODO: empty favorites
+            favoritesGrandparent.children().children().empty();
             favoritesDisplay.hide();
             homeDisplay.show();
             favorites.text('Favorites');
         }  
     })
 
-    function renderVideos() {
+    function renderSearch() {
         // TEST
-        // create new HTML elements
-        var div1 = $('<div class="cell"></div>');
-        var div2 = $('<div class="card"></div>');
-        var para = $('<p></p>').text('test');
-        var favoriteBtn = $('<button class="button favorite">Favorite</button>');
 
-        // query selector for parent element in HTML DOM
-        var videosRow1 = $('#resultsRow1');
+        for (var i=0; i<=8; i++) { //TODO: REPLACE i<=8 with i<=resultsArray.length
+            // create new HTML elements
+            var div1 = $('<div class="cell"></div>');
+            var div2 = $('<div class="card"></div>');
+            var para = $('<p></p>').text('test');
+            var favoriteBtn = $('<button class="button favorite">Favorite</button>');
+    
+            // query selector for parent element in HTML DOM
+            var videosRow1 = $('#resultsRow1');
+            var videosRow2 = $('#resultsRow2');
+            var videosRow3 = $('#resultsRow3');
+    
+            var videoRowArray = [videosRow1, videosRow2, videosRow3];
+    
+            div1.append(div2);
+            div2.append(para);
+            div2.append(favoriteBtn);
 
-        videosRow1.append(div1);
-        div1.append(div2);
-        div2.append(para);
-        div2.append(favoriteBtn);
-
+            if(i<=2) {
+                videoRowArray[0].append(div1);
+            } else if(i<=5) {
+                videoRowArray[1].append(div1);
+            } else if(i<=8) {
+                videoRowArray[2].append(div1);
+            }
+        }
+        
     }
 
-    // TEST
-    renderVideos();
-    renderVideos();
-    renderVideos();
-
     // TODO: fetch youtube API
-        // TODO: create element:
-            // TODO: for loop - every 3rd video needs to start appending to next resultRow
+        // TODO: create resultArray = [] then call renderSearch(); 
 
     // TODO: event listener for favorite button
         // TODO: local storage array for favorites
 
-    // TODO: define renderFavorites function
+    // Define renderFavorites function
+    function renderFavorites() {
+        // TEST
+
+        for (var i=0; i<=8; i++) { //TODO: REPLACE i<=8 with i<=favoritesArray.length
+            // create new HTML elements
+            var div1 = $('<div class="cell"></div>');
+            var div2 = $('<div class="card"></div>');
+            var para = $('<p></p>').text('test');
+            var notesBtn = $('<button class="button notes">Notes</button>');
+    
+            // query selector for parent element in HTML DOM
+            var favoritesRow1 = $('#favoritesRow1');
+            var favoritesRow2 = $('#favoritesRow2');
+            var favoritesRow3 = $('#favoritesRow3');
+    
+            var favoritesRowArray = [favoritesRow1, favoritesRow2, favoritesRow3];
+    
+            div1.append(div2);
+            div2.append(para);
+            div2.append(notesBtn);
+
+            if(i<=2) {
+                favoritesRowArray[0].append(div1);
+            } else if(i<=5) {
+                favoritesRowArray[1].append(div1);
+            } else if(i<=8) {
+                favoritesRowArray[2].append(div1);
+            }
+        }
+        
+    }
 
 })
